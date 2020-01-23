9835bdf4c9448cdd24d85ce9054fa3d6b4cf7647
王資猛 <wangzimeng@wangzimengde-MacBook-Pro.local>
2020-01-23 08:17:15 +0800

    three sine waves

--- start.js
+++ start.js
@@ -2,5 +2,17 @@ const AudioContext = window.AudioContext || window.webkitAudioContext
 const audioCtx = new AudioContext
 
 var sine = audioCtx.createOscillator()
+sine.frequency.value=440;
+sine.type="sine"
 sine.start()
 sine.connect(audioCtx.destination);
+var sineb = audioCtx.createOscillator()
+sineb.frequency.value=523.25;
+sineb.type="sine"
+sineb.start()
+sineb.connect(audioCtx.destination);
+var sinec = audioCtx.createOscillator();
+sinec.frequency.value = 698.46;
+sinec.type = "sine";
+sinec.start();
+sinec.connect(audioCtx.destination);
