73ef93025e2c115ef30c1d2296864b40314f8d97
王資猛 <wangzimeng@wangzimengde-MacBook-Pro.local>
2020-01-23 08:19:31 +0800

    add gainNode

--- start.js
+++ start.js
@@ -1,18 +1,22 @@
 const AudioContext = window.AudioContext || window.webkitAudioContext
 const audioCtx = new AudioContext
 
+var volume = audioCtx.createGain();
+volume.connect(audioCtx.destination);
+
 var sine = audioCtx.createOscillator()
 sine.frequency.value=440;
 sine.type="sine"
 sine.start()
-sine.connect(audioCtx.destination);
+sine.connect(volume);
 var sineb = audioCtx.createOscillator()
 sineb.frequency.value=523.25;
 sineb.type="sine"
 sineb.start()
-sineb.connect(audioCtx.destination);
+sineb.connect(volume);
 var sinec = audioCtx.createOscillator();
 sinec.frequency.value = 698.46;
 sinec.type = "sine";
 sinec.start();
-sinec.connect(audioCtx.destination);
+sinec.connect(volume);
+volume.gain.value=0.2
